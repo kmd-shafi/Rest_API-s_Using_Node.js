064a26042a9b14865e44682d59c1707d8b6ea2a8
kmd-shafi <shaikshafi5259@gmail.com>
2024-05-23 16:03:47 +0530

    install nodemon and HTTP status

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ app
   .route("/api/users/:id")
   .get((req, res) => {
     let id = Number(req.params.id);
+
     const user = users.find((user) => user.id == id);
+    if (!user) {
+      return res.status(404).json({ Error: "User not found" });
+    }
     return res.send(user);
   })
   .patch((req, res) => {
@@ -41,8 +45,21 @@ app.post("/api/users", (req, res) => {
   const body = req.body;
 
   users.push({ ...body, id: users.length + 1 });
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json({
+      status: "fail",
+      message: "please provide all fields",
+    });
+  }
   fs.writeFile("MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({
+    return res.status(201).json({
       status: "success",
       id: users.length,
     });
